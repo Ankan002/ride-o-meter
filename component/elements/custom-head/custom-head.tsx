95dc2dcbdf779561db0383d37cafb66c436896ef
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-09-12 18:26:47 +0530

    feat: :sparkles: add custom head

---
+++ component/elements/custom-head/custom-head.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from "next/head";
+
+interface Props {
+    title: string;
+}
+
+const CustomHead = (props: Props) => {
+    const { title } = props;
+
+    return (
+        <Head>
+            <title>{title}</title>
+            <meta name="description" content="An app that can calculate cab fares with practical prices." />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    );
+};
+
+export default CustomHead;
