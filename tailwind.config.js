c16e1b9d0d87c06e6bc1e15cf544316c20707cb6
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-09-12 18:15:33 +0530

    chore: :lipstick: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
       fontFamily: {
         "manrope": "'Manrope', sans-serif",
         "fira-code": "'Fira Code', monospace"
+      },
+      colors: {
+        primaryDark: "#13131e",
+        secondaryDark: "#10111A",
+        primaryLight: "#F4F6FA",
+        secondaryLight: "#F1F2F6",
+        primaryOrange: "#FC5830",
+        lightGrey: "#B5BAC7",
+        white: "#FFFFFF",
+        primaryYellow: "#F6BA3D",
+        semiTransparent: "rgba(0, 0, 0, 0.6)",
+        primaryGreen: "#08CD92",
+        primaryRed: "#D61C4E"
       }
     },
   },
