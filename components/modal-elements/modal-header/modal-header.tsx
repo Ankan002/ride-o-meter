3d98e72c3e62e7e3184d522756dd8302908c6de2
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-09-14 11:46:53 +0530

    feat: :sparkles: add settings header

---
+++ components/modal-elements/modal-header/modal-header.tsx
@@ -0,0 +1,24 @@
+import React, {MouseEventHandler} from 'react';
+import {IoClose} from "react-icons/io5";
+
+interface Props {
+    title: string;
+    onCloseRequested: MouseEventHandler<HTMLButtonElement>;
+}
+
+const ModalHeader = (props: Props) => {
+    const { titlge, onCloseRequested } = props;
+
+    return (
+        <div className="w-full flex items-center justify-between">
+            <h1 className="flex-1 mr-2 font-manrope md:text-2xl sm:text-xl text-lg font-thin tracking-widest">
+                {title}
+            </h1>
+            <button className="p-1 border-2 border-primaryDark bg-primaryYellow flex items-center justify-center rounded-md" onClick={onCloseRequested}>
+                <IoClose size={25} className="text-primaryDark" />
+            </button>
+        </div>
+    );
+};
+
+export default ModalHeader;
--- components/settings-modal/settings-modal.tsx
+++ components/settings-modal/settings-modal.tsx
@@ -1,5 +1,6 @@
 import React, {Dispatch, SetStateAction} from 'react';
 import Modal from "react-modal";
+import {ModalHeader} from "components/modal-elements";
 
 interface Props {
     isOpen: boolean;
@@ -27,8 +28,8 @@ const SettingsModal = (props: Props) => {
             }}
             closeTimeoutMS={500}
         >
-            <div className="w-full h-[90vh] overflow-y-scroll bg-white border-l-2 border-r-2 border-t-2 border-l-primaryDark border-r-primaryDark border-t-primaryDark flex flex-col rounded-t-lg z-10">
-
+            <div className="w-full h-[90vh] overflow-y-scroll bg-primaryLight border-l-2 border-r-2 border-t-2 border-l-primaryDark border-r-primaryDark border-t-primaryDark flex flex-col rounded-t-lg z-10 px-5 py-2">
+                <ModalHeader title="Settings" onCloseRequested={onRequestedClose} />
             </div>
         </Modal>
     );
